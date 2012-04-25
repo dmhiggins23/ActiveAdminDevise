1f888f07e99e003059ad2409af771f1247aa5437
Daniel Higgins <commerce@lee-higgins.com>
2012-04-25 12:45:43 -0400

    Initial Commit with .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveAdminDevise::Application.config.secret_token = '5cae0283c4f4b1c5385594320942ba3b3906463f03ddf26c63c46ce7d8c2b02becebc02dd1424ddcff3eea2efc55214ed159a0d3569f34c63755b1e38264b581'
